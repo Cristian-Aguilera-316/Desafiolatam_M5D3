e8622c68a92c1df9a91200aa2231b93e10c93147
Cristian-Aguilera-316 <cristian.ap316@gmail.com>
2024-07-14 16:28:40 -0400

    creación del proyecto

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="content">
+    <h1>Bienvenido Maestro Pokemón</h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw3iIiDJQW4HYJk2osK19O3RQ4Hs4MDSF8PQ&s" alt="Pokemon" width="400" height="400" />
+  </div>
+);
+
+export default Home;
