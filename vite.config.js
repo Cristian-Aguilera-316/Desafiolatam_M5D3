c5cd6ebb28d909113d0be49103a073af6a92dd3c
Cristian-Aguilera-316 <cristian.ap316@gmail.com>
2025-12-01 19:20:16 -0300

    Build para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Desafiolatam_M5D3/',
+  base: '/Desafiolatam_M4D2/',
   build: {
     outDir: 'docs'
   }
